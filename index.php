cdc5994556c2ce8d7ae32e474bc5b36c8f47e9c7
cyril jardin <cyilroc60@hotmail.fr>
2018-07-30 10:25:19 +0200

    fonction

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>exo8</title>
+  </head>
+  <body>
+    <p>
+      <?php
+      function number($number1, $number2, $number3){
+        return $number1 + $number2 + $number3;
+      }
+      echo number(18, 36, 50)
+       ?>
+    </p>
+  </body>
+</html>
